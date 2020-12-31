056d4fac0361edd77accd6d279625115cb64ae0b
TeflonTrout <jtkazo@gmail.com>
2020-12-31 14:09:34 -0500

    optimization

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,6 @@ const Home = () => {
         <div className="home-page">
             <h1>Howdy, I'm JT Kazousky and I'm a web developer that specializes in creating clean web apps through React!</h1>
             <h5>(also an avid Vaporwave fan)</h5>
-            {/* <div className="quick-links">
-                <ul>
-                    <li><a href="http://www.github.com/teflontrout">GitHub</a></li>
-                    <li><a href="http://www.github.com/teflontrout">Coffee Blog</a></li>
-                </ul>
-            </div> */}
         </div>
     )
 }
